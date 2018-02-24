1d1d7e11b17623499c7289012ac07a8b25631312
M Saeed Butt <saeedbutt3321@outlook.com>
2018-02-24 01:57:48 +0100

    Api Endpoints

---
+++ Controllers/MakeController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using testing.Models;
+using testing.persistence;
+
+namespace testing.Controllers
+{
+    public class MakeController : Controller
+    {
+        private readonly TestingDbContext context;
+        public MakeController(TestingDbContext context)
+        {
+            this.context = context;
+
+        }
+
+
+        [HttpGet("api/makes")]
+        public async Task<IEnumerable<Make>> GetMakes()
+        {
+            return await  context.Make.Include(m=>m.Model).ToListAsync();
+        }
+
+
+
+    }
+}
