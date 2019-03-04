d4e82ed2f251ae17c3bc6e581273e19643079a93
Xaeed <saeedbutt320@gmail.com>
2019-03-04 13:24:49 +0100

    Update make.service.ts

--- ClientApp/app/Services/make.service.ts
+++ ClientApp/app/Services/make.service.ts
@@ -17,12 +17,12 @@ export class MakeService {
     return this.http.get('/api/makes')
     .map(res=>res.json());
   }
-<<<<<<< HEAD
+
   getModels(){
     return this.http.get('/api/models')
     .map(res=>res.json());
   }
-=======
->>>>>>> ccba1ae434a6ed2893407e699662a1ac4d2d56b1
+
+
   
 }
